e0acbe4c4cbb4e1d93863962fecad38179caf2cc
Your Name <nelliebosch@Nellies-MacPro.lan>
2021-12-02 13:12:36 -0500

    added and styled downloadable resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,5 +1,12 @@
 const Resume = (props) => {
-    return <h1> Welcome to my Portfolio! Click around to check out my projects and personal pages! </h1>
+    return (
+        <>
+            <p>Click Image to Download</p>
+            <a href="https://docs.google.com/document/d/1kCypDermJ82dgx75fJKxgm9zswsNfOiDs4GudD_AgRk" Download>
+                <img id="resume" src="https://imgur.com/CmpdBt3.jpg" alt="resume" ></img>
+            </a>
+        </>
+    )
 };
 
 export default Resume;
