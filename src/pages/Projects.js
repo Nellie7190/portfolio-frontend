b5f43de7c0dfc94259b383e16e4a1b0bab8eea16
Your Name <nelliebosch@Nellies-MacPro.lan>
2022-01-08 20:18:13 -0500

    added searchbar to projs page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -11,6 +11,7 @@ const Projects = (props) => {
       };
     //state to hold projects
     const [projects, setProjects] = useState(null);
+    const [searchTerm, setSearchTerm] = useState('')
 
     //function to make api call
     const getProjectsData = async () => {
@@ -26,21 +27,80 @@ const Projects = (props) => {
     useEffect(() => getProjectsData(), []);
 
     const loaded = () => {
-        return projects.map((project) => (
+        
+        return (
             <div>
-                <h1>{project.name}</h1>
-                <img style={imgStyle} src={project.image} />
-                <p>{project.description}</p>
-                <br />
-                <a href={project.git} target='#'>
-                    <button>Github</button>
-                </a>
-                <a href={project.live} target='#'>
-                    <button>Live Site</button>
-                </a>
+                <input
+                id='search'
+                type='text'
+                placeholder='search'
+                onChange={event=> {
+                    setSearchTerm(event.target.value)
+                }}/>
+                    {projects.filter((val) => {
+                        if (searchTerm == "") {
+                            return val
+                        } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                            return val
+                        }
+                    }).map((val, key) => {
+                        return (
+                            <>
+                            <h1>{val.name}</h1>
+                            <img style={imgStyle} src={val.image} />
+                            <p>{val.description}</p>
+                            <br />
+                            <a href={val.git} target='#'>
+                                <button>Github</button>
+                            </a>
+                            <a href={val.live} target='#'>
+                                <button>Live Site</button>
+                            </a>
+                            </>
+                        )
+                    })}
+                {/* projects.map((project) => (
+                    <div>
+                        
+                    </div>
+                )) */}
             </div>
-        ))
+            )
     }
+
+
+    // const loaded = () => {
+    //     <input
+    //     id='search'
+    //     type='text'
+    //     placeholder='search'
+    //     onChange={event=> {
+    //         setSearchTerm(event.target.value)
+    //     }}>
+    //         {projects.filter((val) => {
+    //             if (searchTerm == "") {
+    //                 return val
+    //             } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+    //                 return val
+    //             }
+    //         })}
+    //     </input>
+    //     return (
+    //     projects.map((project) => (
+    //         <div>
+    //             <h1>{project.name}</h1>
+    //             <img style={imgStyle} src={project.image} />
+    //             <p>{project.description}</p>
+    //             <br />
+    //             <a href={project.git} target='#'>
+    //                 <button>Github</button>
+    //             </a>
+    //             <a href={project.live} target='#'>
+    //                 <button>Live Site</button>
+    //             </a>
+    //         </div>
+    //     )))
+    // }
     return projects ? loaded() : <h1>Loading...</h1>
 };
 
