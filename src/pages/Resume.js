c37e1134c14131a5219bfe4c4bab099890178e84
Your Name <nelliebosch@Nellies-MacPro.lan>
2022-01-11 14:20:46 -0500

    fixed resume pic

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -19,7 +19,7 @@ const Resume = (props) => {
         <Body>
             <p>Click Image to Download</p>
             <a href="https://docs.google.com/document/d/1kCypDermJ82dgx75fJKxgm9zswsNfOiDs4GudD_AgRk" Download>
-                <img id="resume" src="https://imgur.com/CmpdBt3.jpg" alt="resume" ></img>
+                <img id="resume" src="https://imgur.com/zwWQIaR.jpg" alt="resume" ></img>
             </a>
         </Body>
     )
